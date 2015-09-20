62040448ffacf3a73f97fa6c6256e67dae237d7d
Chen YewMing <fusion82@gmail.com>
2015-09-20 13:04:44 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,84 @@
+/* 
+ * File:   main.c
+ * Author: YewMing, Chen (fusion82@gmail.com)
+ *
+ * Created on September 20, 2015, 10:32 AM
+ */
+#define _XTAL_FREQ 1316043
+
+#include <xc.h>
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+
+// CONFIG
+#pragma config FOSC = EXTRC     // Oscillator Selection bits (RC oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = ON       // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3 is digital I/O, HV on MCLR must be used for programming)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+bit toggle = 0;
+
+#define TMR1H_RELOAD 0x80
+
+void interrupt interupt_service_routine(void)
+{
+    // only process timer-triggered interrupts
+    //if(PIE1bits.TMR1IE && PIR1bits.TMR1IF)
+    {
+        TMR1H = TMR1H_RELOAD;
+        PIR1bits.TMR1IF = 0;
+        toggle = toggle ? 0 : 1;
+    }
+}
+
+void mydelay(void)
+{
+    //volatile unsigned int x = 10000;
+    //while (x--);
+    __delay_ms(500);
+    return;
+}
+
+/* Main program loop
+ * 
+ */
+int main(int argc, char** argv)
+{
+    TRISBbits.TRISB0 = 0;
+    //PORTB =  0xAA;
+    
+    // TIMER 1 setting
+    // pre-scale 1:1
+    // oscillator disable
+    // external input sync
+    // timer 1 on
+    T1CON = 0x3;
+    TMR1L = 0x00;
+    TMR1H = TMR1H_RELOAD;
+    PIE1bits.TMR1IE = 1;
+    INTCONbits.PEIE = 1;
+    ei();
+    
+    while (1)
+    {
+        //PORTB = 0xAA;
+        //PORTBbits.RB0 = 0;
+        //mydelay();
+        //PORTB = 0x55;
+        //PORTBbits.RB0 = 1;
+        //mydelay();
+        //PORTBbits.RB0 = toggle ? 0 : 1;
+        PORTBbits.RB0 = toggle;
+    }
+    return (EXIT_SUCCESS);
+}
+
